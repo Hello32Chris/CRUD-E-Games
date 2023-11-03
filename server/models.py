d9b9ef21664b470f75cb90c2d4a90f72f1cff1f4
lorenzo <lorenzomojica561@gmail.com>
2023-11-03 14:59:29 -0400

    description

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from faker import Faker
 
