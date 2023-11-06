4fcc3749c40e4b6f1514e44996efa8293114a4b6
Christopher Orth <christopherorth430@yahoo.com>
2023-11-06 13:25:14 -0500

    added some react

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function App() {
-    return <h1>We Out Here</h1>
-}
 
-export default App
+
+    const [tomato, tumato] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/deeznuts')
+      .then((res) => res.json())
+      .then((data) => tumato(data));
+    }, []);
+
+
+    return (
+        <div>
+            <h1>We Out Here</h1>
+            <Router>
+                <Navbar />
+                <Route exact path="/" element={<Login />} />
+                <Route exact path="/storefront" element={<StoreFront />} />
+                <Route exact path="" element={<Inventory />} />
+            </Router>
+        </div>
+)};
+
+export default App;
