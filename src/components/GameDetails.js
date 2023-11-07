317eb49511acb5eb3e0f82f14dd465d411c2e919
lorenzo <lorenzomojica561@gmail.com>
2023-11-07 18:55:41 -0500

    semi final card animation finished

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import React from "react";
 
-function GameDetails( {description, name, price, quantity, store, type } ){
+function GameDetails( {description, name, price, quantity, store, type, img } ){
 
-    console.log(store)
+    // console.log(store)
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const flipCard = () => {
+        setIsFlipped(!isFlipped)
+    }
+
+    const cardToggle = isFlipped ? "flipped" : ''
 
 
 
     return (
-    <div className={"card-container"}>
-        <div className="card">
-            <div className="card-info">
-            <h2>{name}</h2>
-            <img />
-            <p>{description}</p>
-            <p>{price}</p>
-            <p>{quantity}</p>
-            <p>{type}</p>
+    <div>
+        <div className={`card ${cardToggle}`} onClick={flipCard}>
+            <div className="card-inner">
+                <div className="card-front">
+                    <img src = {img} alt = {name}/>
+                    <h2>{name}</h2>
+                </div>
+                <div className="card-back">
+                    <h2>{name}</h2>
+                    {/* <img /> */}
+                    <p>{description}</p>
+                    <p>{price}</p>
+                    <p>{quantity}</p>
+                    <p>{type}</p>
+                </div>
             {/* <p>{store}</p> Uncomment this line to display the store information */}
             </div>
         </div>
