06e7696a0a91fdeb188a2fcc3f9be1d554c614dc
Christopher Orth <christopherorth430@yahoo.com>
2023-11-08 17:01:53 -0500

    figuring out changes

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,41 +1,48 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import React from "react";
 
-function GameDetails({ description, name, price, quantity, store, type, img }) {
+function GameDetails( {description, name, price, quantity, store, type, img } ){
 
-    const [inCart, setInCart] = useState(false);
     // console.log(store)
     const [isFlipped, setIsFlipped] = useState(false);
+    const [inCart, setInCart] = useState(false);
 
+    const flipCard = () => {
+        setIsFlipped(!isFlipped)
+    }
 
+    const cardToggle = isFlipped ? "flipped" : '';
 
     const addToCart = () => {
         setInCart(!inCart);
+        
     }
 
-    const cardToggle = isFlipped ? "flipped" : ''
-
-
-
     return (
-        <div className={"card-container"}>
-            <div className="card">
-                <div className="card-info">
+        <div className={`card ${cardToggle}`} onClick={flipCard}>
+            <div className="card-inner">
+                <div className="card-front">
+                    <img src = {img} alt = {name}/>
                     <h2>{name}</h2>
-                    <img src={img} alt="" />
+                </div>
+                <div className="card-back">
+                    <h2>{name}</h2>
+                    {/* <img /> */}
                     <p>{description}</p>
                     <p>{price}</p>
                     <p>{quantity}</p>
                     <p>{type}</p>
-                    {/* <p>{store}</p> Uncomment this line to display the store information */}
-                    {inCart ? (
+                </div>
+            {/* <p>{store}</p> Uncomment this line to display the store information */}
+            {inCart ? (
                         <button onClick={addToCart}>Remove from Cart</button>
                     ) : (
                         <button onClick={addToCart}>Add to Cart</button>
                     )}
-                </div>
             </div>
         </div>
     );
 }
 
-export default GameDetails;
+export default GameDetails
+
